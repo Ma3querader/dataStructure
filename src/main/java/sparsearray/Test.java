162ef1147aa5fc8546c4506c335e8af4207fe2b2
Ma3querader <593661332@qq.com>
2021-05-20 11:02:36 +0800

    稀疏数组

---
+++ src/main/java/sparsearray/Test.java
@@ -0,0 +1,74 @@
+package sparsearray;
+
+/**
+ * @Author: panyusheng
+ * @Date: 2021/5/20
+ * @Version 1.0
+ *
+ * 稀疏数组
+ */
+public class Test {
+    public static void main(String[] args) {
+        // 创建一个原始的二维数组 11 * 11
+        // 0: 表示没有棋子， 1 表示 黑子 2 表蓝子
+        int chessArr[][] = new int[11][11];
+        chessArr[1][2] = 1;
+        chessArr[2][3] = 2;
+        System.out.println("原始二维数组");
+        for (int[] row : chessArr) {
+            for (int i : row) {
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+        }
+
+        // 转稀疏数组
+        // 非0个数
+        int sum = 0;
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr[i][j] != 0) {
+                    sum++;
+                }
+            }
+        }
+        // 创建稀疏数组
+        int sparseArr[][] = new int[sum + 1][3];
+        sparseArr[0][0] = 11;
+        sparseArr[0][1] = 11;
+        sparseArr[0][2] = sum;
+        // 将非0的数存进sparseArr
+        int count = 0;
+        for (int i = 0; i < 11; i++) {
+            for (int j = 0; j < 11; j++) {
+                if (chessArr[i][j] != 0) {
+                    count++;
+                    sparseArr[count][0] = i;
+                    sparseArr[count][1] = j;
+                    sparseArr[count][2] = chessArr[i][j];
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("得到稀疏数组");
+        for (int i = 0; i < sparseArr.length; i++) {
+            System.out.printf("%d\t%d\t%d\t\n", sparseArr[i][0], sparseArr[i][1], sparseArr[i][2]);
+        }
+        System.out.println();
+
+        // 稀疏数组恢复成二维
+        // 先创建行列，再恢复非0位
+        int chessArr2[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        System.out.println("恢复后的二维");
+        for (int[] row : chessArr2) {
+            for (int i : row) {
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
