c35f3dede9d960a0cfa5e967beaa27876ffe2b33
Ma3querader <593661332@qq.com>
2021-05-23 02:42:58 +0800

    test

--- src/main/java/linked/SingleLinkedList.java
+++ src/main/java/linked/SingleLinkedList.java
@@ -105,5 +105,63 @@ public class SingleLinkedList {
         }
     }
 
+    /**
+     * 求单链表中有效节点的个数
+     * @return 有效节点数（不含头）
+     */
+    public int getLenth() {
+        if (head.next == null) {
+            return 0;
+        }
+        int len = 0;
+        HeroNode temp = head.next;
+        while (temp != null) {
+            len++;
+            temp = temp.next;
+        }
+        return len;
+    }
+
+    /**
+     * 查找单链表中的倒数第 k 个节点
+     * @param index 倒数第 k 个节点
+     * @return 目标节点
+     */
+    public HeroNode findLastIndexNode(int index) {
+        if (head.next == null) {
+            return null;
+        }
+        int len = this.getLenth();
+        if (index < 0 || index > len) {
+            return null;
+        }
+        HeroNode temp = head.next;
+        // 倒数第 k 个节点 = list(size - index)，从第一个非头节点开始遍历就会得到
+        for (int i = 0; i < len - index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    /**
+     * 单链表反转
+     * @return
+     */
+    public void reversetList() {
+        if(head.next == null || head.next.next == null) {
+            return;
+        }
+        HeroNode temp = head.next;
+        HeroNode next = null;
+        HeroNode reverseHead = new HeroNode(0, "", "");
+        while(temp != null) {
+            next = temp.next;   // 暂存原节点next节点
+            temp.next = reverseHead.next;   // 设置当前节点的next为新链表头的next，达到一直在头插的效果
+            reverseHead.next = temp;    // 当前节点插在新头后，达到反转效果
+            temp = next;    // temp往后遍历
+        }
+        head.next = reverseHead.next;
+    }
+
 
 }
--- src/main/java/linked/SingleLinkedListDemo.java
+++ src/main/java/linked/SingleLinkedListDemo.java
@@ -11,9 +11,18 @@ public class SingleLinkedListDemo {
         list.addByOrder(new HeroNode(1,"第一","第一"));
         list.addByOrder(new HeroNode(3,"第二","第二"));
         list.addByOrder(new HeroNode(2,"第二","第二"));
+        list.addByOrder(new HeroNode(5,"第二","第二"));
         list.update(new HeroNode(4, "333", "333"));
         list.delete(1);
         System.out.println("-----------------");
         list.list();
+        System.out.println("-----------------");
+        System.out.println("有效节点数：" + list.getLenth());
+        System.out.println("倒数第1个节点：" + list.findLastIndexNode(1));
+        System.out.println("-----------------");
+        list.reversetList();
+        list.list();
     }
+
+
 }
